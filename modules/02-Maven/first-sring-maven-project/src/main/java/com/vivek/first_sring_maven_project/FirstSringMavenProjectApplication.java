4cd92db5e3aa396dfe91018cd2e67d3bf09d2f93
Vivek Nehe <viveknehe007@gmail.com>
2026-08-10 19:09:20 +0530

    Created My First Spring Boot project using Maven.

---
+++ modules/02-Maven/first-sring-maven-project/src/main/java/com/vivek/first_sring_maven_project/FirstSringMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.first_sring_maven_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSringMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSringMavenProjectApplication.class, args);
+	}
+
+}
---
+++ modules/02-Maven/first-sring-maven-project/src/test/java/com/vivek/first_sring_maven_project/FirstSringMavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.first_sring_maven_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSringMavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
