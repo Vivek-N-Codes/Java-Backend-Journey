090e78956883cf56f1d5467e8849129f8942da8b
Vivek Nehe <viveknehe007@gmail.com>
2026-08-13 17:47:55 +0530

    Created My spring boot project using Gradle.

---
+++ modules/03-Gradle/gradle_springboot_project/src/main/java/com/vivek/gradle_springboot_project/GradleSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.gradle_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ modules/03-Gradle/gradle_springboot_project/src/test/java/com/vivek/gradle_springboot_project/GradleSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.gradle_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
