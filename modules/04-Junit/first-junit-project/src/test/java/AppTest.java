f8a5a59082a0e080b6fb07cf0ddde214a9727fbd
Vivek Nehe <viveknehe007@gmail.com>
2026-08-16 10:24:05 +0530

    Exploring Junit5 in depth, Learning & practicing it.

--- modules/04-Junit/first-junit-project/src/test/java/AppTest.java
+++ modules/04-Junit/first-junit-project/src/test/java/AppTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -27,8 +29,21 @@ public class AppTest {
         ComputeSquareroot obj = new ComputeSquareroot();
         double actual = obj.squareRoot(16);
         double expected = 4.0;
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "Sqrt computation is incorrect.");
+        // The string displays only when test case fails.
     }
-    
-    
+    @Test
+    void checkSqrtNotEquals() {
+        ComputeSquareroot obj = new ComputeSquareroot();
+        assertNotEquals(8.0, obj.squareRoot(16), () -> "Sqrt computation is incorrect.");        // check for unexpected value should not be the output.
+        // we can use the supplier functional interface as Lambda func.
+    }
+    @Test
+    void checkStringsEqualityTest() {
+        boolean result = StringEquality.checkEquals("Java", "Java");
+        assertTrue(result);
+        // Check for boolean result true, if result = true -> test passes, else -> test fails.
+    }
+
+    // We also have the assertFalse().
 }
