b0ce4c0ffeadbcad372cc625dcc114a45223cd08
Vivek Nehe <viveknehe007@gmail.com>
2026-08-16 10:04:23 +0530

    Exploring the Junit5 in more depth, Learning & Practicing it.

---
+++ modules/04-Junit/first-junit-project/src/main/java/com/unittest/StringEquality.java
@@ -0,0 +1,7 @@
+package com.unittest;
+
+public class StringEquality {
+    public static boolean checkEquals(String str1, String str2){
+        return str1.equalsIgnoreCase(str2);
+    }
+}
--- modules/04-Junit/first-junit-project/src/test/java/AppTest.java
+++ modules/04-Junit/first-junit-project/src/test/java/AppTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,14 +23,34 @@ public class AppTest {
     }
     
     // Test-Driven Development
-    
     @Test
     void ComputeSquarerootTest() {
         ComputeSquareroot obj = new ComputeSquareroot();
         double actual = obj.squareRoot(16);
         double expected = 4.0;
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "squareRoot Computation is incorrect"); 
+        // The string here runs only when test fails.
     }
+
+    @Test
+    void CheckSqrtNotEqulas() {
+        ComputeSquareroot obj = new ComputeSquareroot();
+        assertNotEquals(8.0, obj.squareRoot(16), () -> "Value Matched");    //check for unexpected result should not be match.
+        // Supplier Functional Interface, we can use lambda func here
+    }
+
+    @Test
+    void checkStringsEqualsTest() {
+        boolean result = StringEquality.checkEquals("Java", "Java");
+        assertTrue(result);
+        // Check for boolean values, if true -> test passes, else -> test fails.         
+    }
+
+    // Similarly we have assertFalse()
+
+    
+
+    
     
     
 }
