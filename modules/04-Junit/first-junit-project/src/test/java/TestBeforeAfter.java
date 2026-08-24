caf7cd0aab8cbc572955e84a3928fa1c4b78b7dc
Vivek Nehe <viveknehe007@gmail.com>
2026-08-24 22:26:14 +0530

    Successfully completed the Junit5 Module today.

--- modules/04-Junit/first-junit-project/src/test/java/TestBeforeAfter.java
+++ modules/04-Junit/first-junit-project/src/test/java/TestBeforeAfter.java
@@ -1,13 +1,34 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import com.unittest.ComputeSquareroot;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)  
+// @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestBeforeAfter {
     ComputeSquareroot obj = new ComputeSquareroot();
+
+    TestBeforeAfter(){
+        System.out.println("Test Obj get created before test method");
+    }
+
+    @BeforeAll          // Executes only once for all test cases.
+    static void  beforeALL(){
+        System.out.println("Before all Test cases");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After all Test Cases run.");
+    }
 
     @BeforeEach
     void init(){
@@ -23,4 +44,10 @@ public class TestBeforeAfter {
     void testNotequal(){
         assertNotEquals(8.0, obj.squareRoot(16));
     }
+
+    @AfterEach
+    void destroy(){
+        System.out.println("After Test, close!");
+    }
+
 }
